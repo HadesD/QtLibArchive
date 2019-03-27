6dfbdc06b3ce02c1888c158dfc7bcda152e97221
Dark.Hades <dark.hades.1102@gmail.com>
2019-03-27 18:56:52 +0900

    Add files

---
+++ src/QArchive.cpp
@@ -0,0 +1,6 @@
+#include "QArchive.hpp"
+
+
+QArchive::QArchive()
+{
+}
---
+++ src/QArchive.hpp
@@ -0,0 +1,12 @@
+#ifndef QARCHIVE_HPP
+#define QARCHIVE_HPP
+
+
+class QArchive
+{
+
+  public:
+    QArchive();
+};
+
+#endif // QARCHIVE_HPP
